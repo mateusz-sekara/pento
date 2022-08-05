7b308b674a2eca46cde0fb6dbd221bb2b4b2e18d
Paweł Grodniewicz <grodniewicz@gmail.com>
2022-08-05 17:50:36 +0200

    adding markdown product update

--- lib/pento/catalog.ex
+++ lib/pento/catalog.ex
@@ -101,4 +101,9 @@ defmodule Pento.Catalog do
   def change_product(%Product{} = product, attrs \\ %{}) do
     Product.changeset(product, attrs)
   end
+
+  def markdown_product(%Product{unit_price: unit_price} = product, amount) do
+    Product.price_decrease_changeset(product, %{unit_price: unit_price - amount})
+    |> Repo.update()
+  end
 end
--- lib/pento/catalog/product.ex
+++ lib/pento/catalog/product.ex
@@ -1,6 +1,7 @@
 defmodule Pento.Catalog.Product do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Pento.Catalog.Product
 
   schema "products" do
     field :description, :string
@@ -18,5 +19,20 @@ defmodule Pento.Catalog.Product do
     |> validate_required([:name, :description, :unit_price, :sku])
     |> unique_constraint(:sku)
     |> validate_number(:unit_price, greater_than: 0.0)
+  end
+
+  def price_decrease_changeset(
+    %Product{unit_price: current_unit_price} = product,
+    attrs
+  ) do
+    product
+    |> cast(attrs, [:unit_price])
+    |> validate_change(:unit_price, fn :unit_price, new_unit_price ->
+      if new_unit_price >= current_unit_price do
+        [unit_price: "Price must be lower than current value"]
+      else
+        []
+      end
+    end)
   end
 end
