73c9e1e4841524ef9f8b64d8725a055fd62238ac
Mateusz Sekara <mateusz.sekara@gmail.com>
2022-08-12 13:23:16 +0200

    forms

---
+++ lib/pento/promo.ex
@@ -0,0 +1,12 @@
+defmodule Pento.Promo do
+  alias Pento.Promo.Recipient
+
+  def change_recipient(%Recipient{} = recipient, attrs \\ %{}) do
+    Recipient.changeset(recipient, attrs)
+  end
+
+  def send_promo(_recipient, _attrs) do
+    # no-op placeholder
+    {:ok, %Recipient{}}
+  end
+end
