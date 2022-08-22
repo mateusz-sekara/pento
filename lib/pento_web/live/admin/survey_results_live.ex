b21ec8ca39bb16dc9cca811fbcd22f7701d1395d
Paweł Grodniewicz <grodniewicz@gmail.com>
2022-08-22 18:21:00 +0200

    Improvement introduced by Sekara

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -10,49 +10,14 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
       :ok,
       socket
       |> assign(assigns)
-      |> assign_products_with_average_ratings()
-      |> assign_dataset()
-      |> assign_chart()
-      |> assign_chart_svg()
+      |> assign(:chart, render_chart())
     }
   end
 
-  def assign_products_with_average_ratings(socket) do
-    socket
-    |> assign(:products_with_average_ratings, Catalog.products_with_average_ratings())
-  end
-
-  def assign_dataset(
-    %{
-      assigns: %{
-        products_with_average_ratings: products_with_average_ratings
-      }
-    } = socket
-  ) do
-    socket
-    |> assign(:dataset, make_bar_chart_dataset(products_with_average_ratings))
-  end
-
-  def assign_chart(%{assigns: %{dataset: dataset}} = socket) do
-    socket
-    |> assign(:chart, make_bar_chart(dataset))
-  end
-
-  def assign_chart_svg(%{assigns: %{chart: chart}} = socket) do
-    socket
-    |> assign(:chart_svg, render_bar_chart(chart))
-  end
-
-  def make_bar_chart_dataset(data) do
-    Dataset.new(data)
-  end
-
-  def make_bar_chart(dataset) do
-    Contex.BarChart.new(dataset)
-  end
-
-  def render_bar_chart(chart) do
-    Plot.new(500, 400, chart)
+  def render_chart() do
+    Catalog.products_with_average_ratings()
+    |> Dataset.new()
+    |> Plot.new(Contex.BarChart, 500, 400)
     |> Plot.titles("Product Ratings", "average star ratings per product")
     |> Plot.axis_labels("products", "start")
     |> Plot.to_svg()
