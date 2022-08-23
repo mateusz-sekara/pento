d47c4dc6d6ed78b6abcd2f6517f49fe9fd058dfa
Mateusz Sekara <mateusz.sekara@gmail.com>
2022-08-23 09:01:42 +0200

    Queries in products

--- lib/pento/catalog/product/query.ex
+++ lib/pento/catalog/product/query.ex
@@ -39,8 +39,41 @@ defmodule Pento.Catalog.Product.Query do
 
   def products_with_average_ratings() do
     from p in Product,
-    join: r in assoc(p, :ratings),
-    group_by: p.id,
-    select: {p.name, fragment("?::float", avg(r.stars))}
+      join: r in assoc(p, :ratings),
+      group_by: p.id,
+      select: {p.name, fragment("?::float", avg(r.stars))}
+  end
+
+  def products_by_user_age("18 and under") do
+    products_by_users_age(year(18), year(0))
+  end
+
+  def products_by_user_age("18 to 25") do
+    products_by_users_age(year(25), year(18))
+  end
+
+  def products_by_user_age("25 to 35") do
+    products_by_users_age(year(35), year(25))
+  end
+
+  def products_by_user_age("35 and up") do
+    products_by_users_age(0, year(35))
+  end
+
+  def products_by_users_age(birth_year_min, birth_year_max) do
+    from p in Product,
+      join: r in assoc(p, :ratings),
+      left_join: u in assoc(r, :user),
+      left_join: d in Pento.Survey.Demographic,
+      on: d.user_id == u.id,
+      where:
+        d.year_of_birth >= ^birth_year_min and
+          d.year_of_birth <= ^birth_year_max,
+      group_by: p.id,
+      select: {p.name, fragment("?::float", avg(r.stars))}
+  end
+
+  defp year(period) do
+    DateTime.utc_now().year - period
   end
 end
