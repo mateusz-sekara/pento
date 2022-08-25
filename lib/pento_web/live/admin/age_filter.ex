5865ded29b0ee129758a83d757ef2cf99aab67cc
Mateusz Sekara <mateusz.sekara@gmail.com>
2022-08-25 14:37:16 +0200

    Changing approach to the querying aggregations (#2)
    * Chaing approach to the querying stats:
    * First of all, query.ex is not aware of pretty filter values like "under 18". Instead it expects to get properly build filter as %{max, min}
    * Extracted filter logic to a separate module to encapsulate all logic related to it. Thus, I was able to get rid of hardcoded filter options in html.heex
    * Query is built in a single function in query.ex module. This can be probably extracted into smaller functions, but I don't have a strong opinion here

---
+++ lib/pento_web/live/admin/age_filter.ex
@@ -0,0 +1,24 @@
+defmodule PentoWeb.Admin.AgeFilter do
+  import PentoWeb.Admin.Helpers
+
+  @age_group_filter %{
+    "all" => %{order: 1, val: %{}},
+    "18 and under" => %{order: 2, val: %{min: year(18)}},
+    "18 to 25" => %{order: 3, val: %{min: year(25), max: year(15)}},
+    "25 to 35" => %{order: 4, val: %{min: year(35), max: year(25)}},
+    "35 and up" => %{order: 5, val: %{max: year(35)}}
+  }
+
+  def options do
+    Map.to_list(@age_group_filter)
+    |> Enum.sort_by(fn {_, %{order: order}} -> order end)
+    |> Enum.map(fn {key, _} -> key end)
+  end
+
+  def filter_value(key) do
+    Map.get(@age_group_filter, key, %{})
+    |> Map.get(:val, %{})
+  end
+
+  def default_option, do: "all"
+end
